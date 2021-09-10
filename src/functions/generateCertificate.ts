d3e2366de3a3b22200bc4d58f8f2d58e398e4588
Anderson <anderson.dasilva@osf.digital>
2021-09-10 12:46:59 -0400

    feature: finished generateCertificate feature to generate PDF

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,9 +1,30 @@
+import * as path from "path";
+import {readFileSync} from "fs";
 import { document } from "src/utils/dynamodbClient";
+import * as handlebars from "handlebars";
+import dayjs from "dayjs";
+
+import chromium from "chrome-aws-lambda";
 
 interface ICreateCertificateDTO {
     id: string;
     name: string;
     grade: string;
+}
+
+interface ITemplate {
+    id: string;
+    name: string;
+    grade: string;
+    date: string;
+    medal: string;
+}
+
+const compile = async function(data: ITemplate){
+    const filePath = path.join(process.cwd(), "src", "templates", "certificate.hbs")
+    const html = readFileSync(filePath,'utf-8');
+    
+    return handlebars.compile(html)(data);
 }
 
 export const handle = async (event) => {
@@ -17,6 +38,40 @@ export const handle = async (event) => {
             grade
         }
     }).promise();
+
+    const medalPath = path.join(process.cwd(), "src", "templates", "selo.png");
+    const medal = readFileSync(medalPath, "base64");
+
+    const data: ITemplate = {
+        date: dayjs().format("DD/MM/YYYY"),
+        grade,
+        name,
+        id,
+        medal
+    }
+
+    const content = await compile(data);
+
+    const browser = await chromium.puppeteer.launch({
+        headless: true,
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath
+    })
+
+    const page = await browser.newPage();
+    
+    await page.setContent(content);
+
+    const pdf = await page.pdf({
+        format: "a4",
+        landscape: true,
+        path: process.env.IS_OFFLINE ? "certificate.pdf" : null,
+        printBackground: true,
+        preferCSSPageSize: true,
+    });
+
+    await browser.close();
 
     return {
         statusCode: 201,
